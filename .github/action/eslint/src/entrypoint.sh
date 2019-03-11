1e24c1deca91c248997b9dce3ea2f6fb9fe78a91
Crawl <icrawltogo@gmail.com>
2019-03-11 20:00:30 +0100

    chore: change shell script

--- .github/action/eslint/src/entrypoint.sh
+++ .github/action/eslint/src/entrypoint.sh
@@ -4,10 +4,10 @@ set -e
 
 echo "## Installing modules..."
 if [ -f yarn.lock ]; then
-	setup="yarn --production=false &&"
+	yarn --production=false
 else
-	setup="NODE_ENV=development npm install &&"
+	NODE_ENV=development npm install
 fi
 
 echo "## Running ESLint"
-sh -c "$setup node /action/eslint/src/index.js"
+node /action/eslint/src/index.js
